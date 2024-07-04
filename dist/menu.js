2a7feda33f1842ddef6d0bd8ef2962a0a6ca4164
송이현 <songehyun00@gmail.com>
2024-07-04 11:43:49 +0900

    BasicData를 이용해서 안의 내용만 바꾸는 코드 완성

--- dist/menu.js
+++ dist/menu.js
@@ -2,8 +2,30 @@
 
 var root = document.getElementById("root");
 console.log(root);
-var liTags = function liTags() {
-  var result = "<li>목록</li>";
+var anchorTags = function anchorTags(inherf, content) {
+  var result = "<a href=\"".concat(inherf, "\">").concat(content, "</a>");
   return result;
 };
-root.innerHTML = "\n<ul>\n  ".concat(liTags, "\n</ul>\n");
+var liTags = function liTags(children) {
+  var result = "<li>".concat(children, "</li>");
+  return result;
+};
+var basicData = {
+  jeamin: "재민",
+  hoyoung: "호영",
+  ujin: "유진",
+  jiyoon: "지윤"
+};
+var totalElement = function totalElement(object) {
+  var result = "";
+  for (var key in object) {
+    // TypeScript가 key가 BasicData의 키인 것을 알게 함
+    var value = object[key];
+    result += liTags(anchorTags(key, value));
+  }
+  return result;
+};
+
+// let result = liTags(anchorTags(`#${object.jeamin}`, object.jeamin));
+
+root.innerHTML = "\n<ul>\n  ".concat(totalElement(basicData), "\n</ul>\n");
--- src/menu.ts
+++ src/menu.ts
@@ -1,18 +1,44 @@
-const root = document.getElementById("root")!;
+const root = document.getElementById("root") as HTMLDivElement;
 console.log(root);
 
-const anchorTags = () => {
-  let result = '<a herf="#">메뉴</a>';
+const anchorTags = (inherf: string, content: string): string => {
+  let result = `<a href="${inherf}">${content}</a>`;
   return result;
 };
 
-const liTags = (children: string) => {
+const liTags = (children: string): string => {
   let result = `<li>${children}</li>`;
   return result;
 };
 
+interface BasicData {
+  jeamin: "재민";
+  hoyoung: "호영";
+  ujin: "유진";
+  jiyoon: "지윤";
+}
+
+const basicData: BasicData = {
+  jeamin: "재민",
+  hoyoung: "호영",
+  ujin: "유진",
+  jiyoon: "지윤",
+};
+
+const totalElement = (object: BasicData): string => {
+  let result = "";
+  for (let key in object) {
+    // TypeScript가 key가 BasicData의 키인 것을 알게 함
+    const value = object[key as keyof BasicData];
+    result += liTags(anchorTags(key, value));
+  }
+  return result;
+};
+
+// let result = liTags(anchorTags(`#${object.jeamin}`, object.jeamin));
+
 root.innerHTML = `
 <ul>
-  ${liTags(anchorTags())}
+  ${totalElement(basicData)}
 </ul>
 `;
